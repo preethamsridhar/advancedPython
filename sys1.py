1070a086ae3ecdea05c9ca6fc6898038049fadcb
Preetham Sridhar <preetham.prem2325@gmail.com>
2019-04-30 15:34:09 +0530

    experimenting with sys module

---
+++ sys1.py
@@ -0,0 +1,42 @@
+import sys
+# =============================================================================
+# for i in range(len(sys.argv)):
+#     if i == 0:
+#         print("Function name: ", sys.argv[0])
+#     else: 
+#         print("%d. argument: %s" %(i, sys.argv[i]))
+#     
+#     
+# =============================================================================
+# =============================================================================
+# for i in (sys.stdin, sys.stdout, sys.stderr):
+#     print(i)
+# =============================================================================
+
+# =============================================================================
+# print("Going via stdout")
+# sys.stdout.write("Another way to do it!")
+# x = input("read value via stdin: ")
+# print(x)
+# 
+# print("type in value: "dsfd) 
+# sys.stdin.readline()[:-1]
+# 
+# =============================================================================
+# =============================================================================
+# 
+# while True:
+#     print("Yet anpother iteration")
+#     try:
+#         number = input("Enter a number: ")
+#     except EOFError:
+#         print("\nciao")
+#         break
+#     else:
+#         number = int(number)
+#         if number == 0:
+#             print("0 has no inverse", file=sys.stderr)
+#         else: 
+#             print("inverse of %d is %f" %(number, 1.0/number))
+# =============================================================================
+
